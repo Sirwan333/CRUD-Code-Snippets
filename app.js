27e162df3a7736cf28a2db6640498dc7be0bbcef
Sirwan <sr222qn@student.lnu.se>
2020-12-18 22:05:19 +0100

    Added basic architcture to the application

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello Express!")
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var hbs = require("express-hbs")
+
+var app = express()
+
+app.engine("hbs", hbs.express4({
+    defaultLayout: path.join(__dirname, "views", "layouts", "default")
+}))
+app.set("view engine", "hbs")
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/", require("./routes/home"))
+app.set("views", path.join(__dirname, "views"))
+app.listen(3000, () => { console.log("Server is running at http://localhost:3000")})
